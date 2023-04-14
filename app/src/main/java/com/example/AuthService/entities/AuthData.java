e365c1b8c7e1d1d163e8465d7275a3f809dedf7c
Mohnish Reddy <mscreddy09@gmail.com>
2023-04-14 17:41:04 +0530

    Modify auth data entity

--- app/src/main/java/com/example/AuthService/entities/AuthData.java
+++ app/src/main/java/com/example/AuthService/entities/AuthData.java
@@ -1,5 +1,6 @@
 package com.example.AuthService.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -10,11 +11,16 @@ import lombok.Data;
 @Table(name = "user_auth_data")
 public class AuthData {
     @Id
+    @Column(nullable = false, unique = true, length = 100, columnDefinition = "VARCHAR(100)")
     private String username;
+    @Column(nullable = false)
     private String firstName;
     private String lastName;
+    @Column(nullable = false, unique = true)
     private String phone;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
     private String createdAt;
     private String lastUpdatedAt;
